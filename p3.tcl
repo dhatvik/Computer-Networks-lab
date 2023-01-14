6fb4955101638c7b153ec9c65fbb62c3894068ee
Dhatvik.M.P <75828380+dhatvik@users.noreply.github.com>
2023-01-14 09:26:35 -0500

    Update p3.tcl

--- p3.tcl
+++ p3.tcl
@@ -1,3 +1,7 @@
+Simulate a four-node point-to-point network, and connect the links as follows: n0->n2, n1-
+>n2 and n2->n3. Apply TCP agent changing the parameters and determine the number of
+packets sent/received by TCP/UDP
+
 set ns [new Simulator]
 set nf [open p2.nam w]
 $ns namtrace-all $nf
